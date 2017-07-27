d5ec198837773e5e43e1374b070a1710befff8d0
skov.simon3 <skov.simon3@gmail.com>
2017-07-27 14:58:57 +0200

    person liste

---
+++ DM_Skills/DM_Skills/Converters/CompareValuesConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DM_Skills.Converters
+{
+    class CompareValuesBoolConverter : CompareValuesConverter<bool> { };
+
+    class CompareValuesConverter<T> : IValueConverter, IMultiValueConverter
+    {
+        public T TrueValue { get; set; }
+        public T FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string[] paramSplit = ((string)parameter).Split(' ', ',');
+            bool isEqual = System.Convert.ToBoolean(paramSplit[1]); ;
+            bool result = false;
+
+            if (value is double)
+            {
+                double paramVal = System.Convert.ToDouble(paramSplit[0]);
+                result = ((double)value == paramVal && isEqual) || ((double)value != paramVal && !isEqual);
+            }
+
+            return result ? TrueValue : FalseValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = false;
+            parameter = parameter == null ? "" : parameter;
+
+            if (((string)parameter).ToUpper() == "OR")
+            {
+                foreach (var item in values)
+                {
+                    result = (bool)item;
+
+                    if (result)
+                        break;
+                }
+            }
+            else if (((string)parameter).ToUpper() == "AND")
+            {
+                result = values.Length > 0;
+
+                foreach (var item in values)
+                {
+                    result = result && (bool)item;
+                }
+            }
+
+            return result ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DM_Skills/DM_Skills/Scripts/Helper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DM_Skills.Scripts
+{
+    class Helper
+    {
+        public static T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+
+            T parent = VisualTreeHelper.GetParent(child) as T;
+
+            if (parent != null)
+                return parent;
+            else
+                return FindParent<T>(parent);
+        }
+
+        public static Visual FindAncestor(Visual child, Type typeAncestor)
+
+        {
+
+            DependencyObject parent = VisualTreeHelper.GetParent(child);
+
+            while (parent != null && !typeAncestor.IsInstanceOfType(parent))
+
+            {
+
+                parent = VisualTreeHelper.GetParent(parent);
+
+            }
+
+            return (parent as Visual);
+        }
+    }
+}
