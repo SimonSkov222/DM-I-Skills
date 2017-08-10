1941994afa091b344a8de616ffee35e3b1f88595
shsk <shsk@TEC-A-PC4>
2017-08-10 08:09:35 +0200

    uu

---
+++ DM_Skills/DM_Skills/Models/SettingsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM_Skills.Models
+{
+    class SettingsModel
+    {
+        //private int _
+        public int TableCnt { get; set; }
+    }
+}
