fe80caed6a6c44b15442bd680b38c3bcc7210f2b
shsk <shsk@TEC-A-PC4>
2017-08-11 13:26:06 +0200

    gg

--- DM_Skills/DM_Skills/Views/sog pa tider.xaml.cs
+++ DM_Skills/DM_Skills/Views/sog pa tider.xaml.cs
@@ -79,115 +79,28 @@ namespace DM_Skills.Views
 
 
             Itemsss = new List<Models.TableModelN>();
-
-            Itemsss.Add(new Models.TableModelN()
+            for (int i = 0; i < 20; i++)
             {
-                School = new Models.SchoolModel() { Name = "Skole " },
-                Location = new Models.LocationModel() { Name = "Frederiksberg" },
-                Persons = new ObservableCollection<Models.PersonModel>() {
-                    new Models.PersonModel() { Name = "Person 1 "},
-                    new Models.PersonModel() { Name = "Person 2 "}
-                },
-                Team = new Models.TeamModel()
+                Itemsss.Add(new Models.TableModelN()
                 {
-                    Date = "01-08-2017 ",
-                    Class = "7A ",
-                    Time = "14:00:12 ",
-                    ID = 1
-                }
+                    School = new Models.SchoolModel() { Name = "Skole "+(i +1) },
+                    Location = new Models.LocationModel() { Name = "Frederiksberg" },
+                    Persons = new ObservableCollection<Models.PersonModel>() {
+                        new Models.PersonModel() { Name = "Person 1 "},
+                        new Models.PersonModel() { Name = "Person 2 "},
+                        new Models.PersonModel() { Name = "Person 2 "},
+                        new Models.PersonModel() { Name = "Person 2 "}
+                    },
+                    Team = new Models.TeamModel()
+                    {
+                        Date = "01-08-2017 ",
+                        Class = "7A ",
+                        Time = "14:00:12 ",
+                        ID = 1
+                    }
 
-            });
-
-            Itemsss.Add(new Models.TableModelN()
-            {
-                School = new Models.SchoolModel() { Name = "Skole 2 " },
-                Location = new Models.LocationModel() { Name = "Lokation 2 " },
-                Persons = new ObservableCollection<Models.PersonModel>() {
-                    new Models.PersonModel() { Name = "Person 2 1 "},
-                    new Models.PersonModel() { Name = "Person 2 2 "}
-                },
-                Team = new Models.TeamModel()
-                {
-                    Date = "02-08-2017 ",
-                    Class = "8A ",
-                    Time = "13:00:12 ",
-                    ID = 2
-                }
-
-            });
-
-            Itemsss.Add(new Models.TableModelN()
-            {
-                School = new Models.SchoolModel() { Name = "Skole 3 " },
-                Location = new Models.LocationModel() { Name = "Lokation 3 " },
-                Persons = new ObservableCollection<Models.PersonModel>() {
-                    new Models.PersonModel() { Name = "Person 3 1 "},
-                    new Models.PersonModel() { Name = "Person 3 2 "}
-                },
-                Team = new Models.TeamModel()
-                {
-                    Date = "03-08-2017 ",
-                    Class = "8b ",
-                    Time = "15:00:12 ",
-                    ID = 3
-                }
-
-            });
-
-            Itemsss.Add(new Models.TableModelN()
-            {
-                School = new Models.SchoolModel() { Name = "Skole 4 " },
-                Location = new Models.LocationModel() { Name = "Lokation 4 " },
-                Persons = new ObservableCollection<Models.PersonModel>() {
-                    new Models.PersonModel() { Name = "Person 4 1 "},
-                    new Models.PersonModel() { Name = "Person 4 2 "}
-                },
-                Team = new Models.TeamModel()
-                {
-                    Date = "04-08-2017 ",
-                    Class = "8c ",
-                    Time = "11:00:12 ",
-                    ID = 4
-                }
-
-            });
-
-            Itemsss.Add(new Models.TableModelN()
-            {
-                School = new Models.SchoolModel() { Name = "Skole 5 " },
-                Location = new Models.LocationModel() { Name = "Lokation 5 " },
-                Persons = new ObservableCollection<Models.PersonModel>() {
-                    new Models.PersonModel() { Name = "Person 5 1 "},
-                    new Models.PersonModel() { Name = "Person 5 2 "}
-                },
-                Team = new Models.TeamModel()
-                {
-                    Date = "02-08-2017 ",
-                    Class = "9A ",
-                    Time = "13:00:12 ",
-                    ID = 5
-                }
-
-            });
-
-            Itemsss.Add(new Models.TableModelN()
-            {
-                School = new Models.SchoolModel() { Name = "Skole 6 " },
-                Location = new Models.LocationModel() { Name = "Lokation 6 " },
-                Persons = new ObservableCollection<Models.PersonModel>() {
-                    new Models.PersonModel() { Name = "Person 6 1 "},
-                    new Models.PersonModel() { Name = "Person 6 2 "}
-                },
-                Team = new Models.TeamModel()
-                {
-                    Date = "02-06-2017 ",
-                    Class = "8x ",
-                    Time = "14:00:12 ",
-                    ID = 6
-                }
-
-            });
-
+                });
+            }
 
             //debug_grid.DataContext = debug_item;
             Loaded += Sog_pa_tider_Loaded;
@@ -195,7 +108,7 @@ namespace DM_Skills.Views
 
         private void Sog_pa_tider_Loaded(object sender, RoutedEventArgs e)
         {
-            BindingOperations.GetBindingExpressionBase(results, ListView.ItemsSourceProperty);
+            //BindingOperations.GetBindingExpressionBase(results, ListView.ItemsSourceProperty);
         }
 
         private void results_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
@@ -211,39 +124,62 @@ namespace DM_Skills.Views
             }
         }
 
-        private void Result_CheckAll(object sender, RoutedEventArgs e)
+        private void SearchList_CheckAll(object sender, RoutedEventArgs e)
         {
             if (Print_ == 1) return;
 
             if(((CheckBox)sender).IsChecked ?? false)
-                results.SelectAll();
+                searchList.SelectAll();
             else
-                results.SelectedIndex = -1;
+                searchList.SelectedIndex = -1;
         }
 
         
-        private void Results_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void SearchList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Print_ = 1;
-            printCheckAll.IsChecked = results.SelectedItems.Count == results.Items.Count;
+            printCheckAll.IsChecked = searchList.SelectedItems.Count == searchList.Items.Count;
             Print_ = 0;
         }
 
 
-        private bool codeReSizeColumn = false;
-        private void results_SizeChanged(object sender, SizeChangedEventArgs e)
+        //private bool codeReSizeColumn = false;
+        //private void results_SizeChanged(object sender, SizeChangedEventArgs e)
+        //{
+        //    if (!((GridViewColumnHeader)sender).IsLoaded)
+        //        return;
+
+        //    if (codeReSizeColumn)
+        //        return;
+
+        //    codeReSizeColumn = true;
+        //    ((GridViewColumnHeader)sender).Width = e.PreviousSize.Width;
+        //    codeReSizeColumn = false;
+        //    //((GridViewColumnHeader)sender).SizeChanged += results_SizeChanged;
+        //    //Console.WriteLine("Size! {0} == {1} == {2}", e.NewSize, e.PreviousSize, sender.GetType());
+        //}
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
-            if (!((GridViewColumnHeader)sender).IsLoaded)
-                return;
+            var scrollviewer = (ScrollViewer)Scripts.Helper.FindAncestor(this, typeof(ScrollViewer));
 
-            if (codeReSizeColumn)
-                return;
+            scrollviewer.ScrollChanged += (o, ee) =>
+            {
+                var margin = ((Grid)sender).Margin;
+                margin.Top = ((ScrollViewer)o).VerticalOffset;
+                ((Grid)sender).Margin = margin;
+            };
+        }
 
-            codeReSizeColumn = true;
-            ((GridViewColumnHeader)sender).Width = e.PreviousSize.Width;
-            codeReSizeColumn = false;
-            //((GridViewColumnHeader)sender).SizeChanged += results_SizeChanged;
-            //Console.WriteLine("Size! {0} == {1} == {2}", e.NewSize, e.PreviousSize, sender.GetType());
+        private void Button_Print_Click(object sender, RoutedEventArgs e)
+        {
+            var wPrint = new Views.Udskriv();
+            wPrint.Owner = App.Current.MainWindow;
+            wPrint.ShowInTaskbar = false;
+            wPrint.ResizeMode = ResizeMode.NoResize;
+            wPrint.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            wPrint.ShowDialog();
         }
     }
 }
