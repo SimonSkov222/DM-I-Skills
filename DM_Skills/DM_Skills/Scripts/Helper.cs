07c3e12d19be8e2e6bcfa6ff2b9889afa830d9a1
Kim Danborg <kim.danborg@hotmail.com>
2017-08-23 08:45:40 +0200

    sdfsdf

--- DM_Skills/DM_Skills/Scripts/Helper.cs
+++ DM_Skills/DM_Skills/Scripts/Helper.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace DM_Skills.Scripts
 {
@@ -95,6 +96,60 @@ namespace DM_Skills.Scripts
             return output;
         }
 
+        public static void CreateBitmapFromVisual(Visual target, string fileName)
+        {
+            if (target == null || string.IsNullOrEmpty(fileName))
+            {
+                return;
+            }
 
+            Rect bounds = VisualTreeHelper.GetDescendantBounds(target);
+
+            RenderTargetBitmap renderTarget = new RenderTargetBitmap((Int32)bounds.Width, (Int32)bounds.Height, 96, 96, PixelFormats.Pbgra32);
+
+            DrawingVisual visual = new DrawingVisual();
+
+            using (DrawingContext context = visual.RenderOpen())
+            {
+                VisualBrush visualBrush = new VisualBrush(target);
+                context.DrawRectangle(visualBrush, null, new Rect(new Point(), bounds.Size));
+            }
+
+            renderTarget.Render(visual);
+            PngBitmapEncoder bitmapEncoder = new PngBitmapEncoder();
+            bitmapEncoder.Frames.Add(BitmapFrame.Create(renderTarget));
+            using (System.IO.Stream stm = System.IO.File.Create(fileName))
+            {
+                bitmapEncoder.Save(stm);
+            }
+        }
+
+        public static void BackupBitmap(Visual target)
+        {
+            string folder = Directory.GetCurrentDirectory() + @"\Backup\";
+
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            string filename = $"{DateTime.Now.ToShortDateString()} " +
+                $"{DateTime.Now.ToLongTimeString().Replace(":", ".")} ";
+            string number = "";
+            int i = 1;
+
+            while (File.Exists(folder + filename + number + ".jpg"))
+            {
+                number = $"({i})";
+                i++;
+            }
+
+            string fullFilename = folder + filename + number + ".jpg";
+
+            CreateBitmapFromVisual(target, fullFilename);
+
+
+        }
     }
 }
